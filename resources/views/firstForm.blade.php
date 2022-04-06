3cef1e40a48c26a8b581f42e94017981d15ce992
Carlitos Bernardino <carlitosbernardino@Carlitoss-MacBook-Pro.local>
2022-04-06 18:48:35 +0800

    testing

---
+++ resources/views/firstForm.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<form method="POST" action="/enter-names">
+<div class="input-group flex-nowrap" name="name1">
+  <span class="input-group-text" id="addon-wrapping">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+
+<div class="input-group flex-nowrap" name="name2">
+  <span class="input-group-text" id="addon-wrapping">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+
+<div class="input-group flex-nowrap" name="name3">
+  <span class="input-group-text" id="addon-wrapping">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+
+
+ <div class="input-group flex-nowrap" name="name4">
+  <span class="input-group-text" id="addon-wrapping">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+
+ <div class="input-group flex-nowrap" name="name5">
+  <span class="input-group-text" id="addon-wrapping">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+
+<div class="rows">
+  <button class="">Submit</button>
+</div>
+
+</div>
+</form>
+</body>
+</html>
